0ee674c3d90397b7b6f6dad1c92a1b588f99c1b4
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-05-26 17:00:03 -0300

    aaaa fuck me sideways!

--- hosts/ThiagoServer/modules/multimedia/default.nix
+++ hosts/ThiagoServer/modules/multimedia/default.nix
@@ -46,76 +46,9 @@
         enable = true;
         openFirewall = true;
       };
-      declarative = true;
+      declarative = false;
       dataDir = "/media/deluge";
       openFirewall = true;
-      config = {
-        send_info = false;
-        info_sent = 0;
-        daemon_port = 58846;
-        allow_remote = false;
-        pre_allocate_storage = true;
-        download_location = "/media/downloads/";
-        listen_ports = [
-          6881
-          6889
-        ];
-        random_port = true;
-        listen_random_port = true;
-        listem_use_sys_port = true;
-        outgoing_ports = [
-          0
-          0
-        ];
-        random_outgoing_ports = true;
-        copy_torrent_files = false;
-        del_copy_torrent_file = false;
-        torrentfiles_location = "/media/downloads/.torrent/";
-        plugins_location = "/media/deluge/.config/deluge/plugins/";
-        prioritize_first_last_pieces = false;
-        sequential_download = false;
-        dht = false;
-        upnp = false;
-        natpmp = false;
-        utpex = false;
-        lsd = false;
-        enc_in_policy = 1;
-        enc_out_policy = 1;
-        enc_level = 2;
-        max_connections_global = 200;
-        max_upload_speed = "-1.0";
-        max_download_speed = "-1.0";
-        max_upload_slots_global = 4;
-        max_half_open_connections = 50;
-        max_connections_per_second = 20;
-        ignore_limits_on_local_network = true;
-        max_connections_per_torrent = -1;
-        max_upload_slots_per_torrent = -1;
-        max_upload_speed_per_torrent = -1;
-        max_download_speed_per_torrent = -1;
-        enabled_plugins = [ "Label" ];
-        add_paused = false;
-        max_active_seeding = 5;
-        max_active_downloading = 3;
-        max_active_limit = 8;
-        dont_count_slow_torrents = false;
-        queue_new_to_top = false;
-        stop_seed_at_ratio = false;
-        remove_seed_at_ratio = false;
-        stop_seed_ratio = "2.0";
-        share_ratio_limit = "2.0";
-        seed_time_ratio_limit = "7.0";
-        seed_time_limit = 180;
-        auto_managed = true;
-        move_completed = false;
-        move_completed_path = "/media/downloads/";
-      };
-      authFile = pkgs.writeTextFile {
-        name = "deluge-auth";
-        text = ''
-          localclient:deluge:10
-        '';
-      };
     };
   };
 }
