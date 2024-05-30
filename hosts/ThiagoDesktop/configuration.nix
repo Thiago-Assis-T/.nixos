d76c07b8854f842644fa8a633395fabf844256c5
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-05-29 22:25:13 -0300

    stuff

--- hosts/ThiagoDesktop/configuration.nix
+++ hosts/ThiagoDesktop/configuration.nix
@@ -18,7 +18,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  programs.powerManagement = {
+  powerManagement = {
     enable = true;
     powertop.enable = lib.mkForce false;
   };
