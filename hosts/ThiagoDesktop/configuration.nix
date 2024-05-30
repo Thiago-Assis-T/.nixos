53447dce48e7bd0d8985f174ba8a959b91e1299f
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-05-29 22:27:04 -0300

    flakes...

--- hosts/ThiagoDesktop/configuration.nix
+++ hosts/ThiagoDesktop/configuration.nix
@@ -15,8 +15,6 @@
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   powerManagement = {
     enable = true;
