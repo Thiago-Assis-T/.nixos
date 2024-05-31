7e6e19bdf749c576d89029dc65837586b302018d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-05-31 15:29:41 -0300

    hardware accel

--- hosts/ThiagoServer/modules/multimedia/default.nix
+++ hosts/ThiagoServer/modules/multimedia/default.nix
@@ -5,8 +5,9 @@
   };
   hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
-      intel-media-driver
       vaapiIntel
       vaapiVdpau
       libvdpau-va-gl
