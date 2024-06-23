8c97e5fe1f4f2e0e9f506054e966c91000aa04a0
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-06-23 19:19:37 -0300

    hyprland...

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -16,15 +16,44 @@ in
       systemd.enable = true;
 
       settings = {
+        input = {
+          kb_layout = "br";
+          follow_mouse = 1;
+          touchpad = {
+            natural_scroll = true;
+          };
+        };
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+          border_size = 2;
+          layout = "dwindle";
+        };
         decoration = {
+          rounding = 0;
+          blur = {
+            enabled = true;
+            size = 3;
+
+            passes = 1;
+          };
+          drop_shadow = true;
           shadow_offset = "0 5";
+          shadow_range = 4;
+          shadow_render_power = 3;
           "col.shadow" = "rgba(00000099)";
+        };
+        animations = {
+          enabled = false;
         };
 
         "$mod" = "SUPER";
         bind = [
           "$mod,W,exec,floorp"
-          "$mod,Return,exec,wezterm"
+          "$mod,Q,killactive"
+          "$mod,Return,exec,kgx"
+          "$mod,M,exit"
+          "$mod,V,togglefloating"
         ];
       };
     };
