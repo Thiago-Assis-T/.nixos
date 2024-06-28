0b20921d126cc1f749beb2406b924eadb8e46bdb
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-06-27 23:57:08 -0300

    looking better...

--- hosts/modules/stylix/default.nix
+++ hosts/modules/stylix/default.nix
@@ -1,15 +1,33 @@
 { pkgs, ... }:
+let
+  inputImage = ../../../wallpapers/tokyo_rainy_road.jpg;
+  brightness = "-30";
+  contrast = "0";
+  fillColor = "black";
+in
 {
   stylix = {
     enable = true;
     autoEnable = true;
     polarity = "dark";
-    image = ../../../wallpapers/Guardian.png;
-    targets = { };
+    image = pkgs.runCommand "dimmed-background.png" { } ''
+      ${pkgs.imagemagick}/bin/convert "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
+    '';
+    targets = {
+      nixvim = {
+        transparent_bg = {
+          main = true;
+          sign_column = true;
+        };
+      };
+    };
     cursor = {
       name = "BreezeX-RosePine-Linux";
       package = pkgs.rose-pine-cursor;
       size = 24;
+    };
+    opacity = {
+      terminal = 0.8;
     };
     fonts = {
       sizes = {
