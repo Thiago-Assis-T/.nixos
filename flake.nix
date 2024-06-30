9938f70862aa35df7a887af49f5bb26aa3488f1d
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-06-29 21:22:16 -0300

    no need for small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Personal Homelab and Desktop configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
