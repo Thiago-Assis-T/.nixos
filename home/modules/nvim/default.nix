16e72a600a887fc6641f69ccd87b9a4b044393db
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-07-15 17:33:19 -0300

    moving to a separate file

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -5,7 +5,6 @@ in {
     enable = lib.mkEnableOption (lib.mdDoc "my-nvim");
   };
   config = lib.mkIf cfg.enable {
-
     programs.neovim = {
       enable = true;
       defaultEditor = true;
@@ -22,24 +21,7 @@ in {
         stylua
         lua54Packages.luacheck
       ];
-      extraLuaConfig = ''
-        vim.g.mapleader = ' '
-        vim.g.maplocalleader = ' '
-        vim.opt.termguicolors = true
-        vim.g.have_nerd_font = true
-        vim.opt.relativenumber = true
-        vim.opt.cursorline = true
-        vim.opt.hlsearch = true
-        vim.opt.tabstop = 2
-        vim.opt.shiftwidth = 2
-        vim.opt.updatetime = 300
-        vim.opt.scrolloff = 10
-        vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-      '';
+      extraLuaConfig = builtins.readFile ./lua/extraConfig.lua 
       plugins = [
         pkgs.vimPlugins.plenary-nvim
         {
---
+++ home/modules/nvim/lua/extraConfig.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.termguicolors = true
+vim.g.have_nerd_font = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.updatetime = 300
+vim.opt.scrolloff = 10
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
