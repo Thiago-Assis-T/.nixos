95f8b24218b7c17c4a2f4bc36691e2b99149808a
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-07-14 21:58:19 -0300

    adding treesitter

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -27,28 +27,30 @@ in
 				vim.g.have_nerd_font = true
 				vim.opt.relativenumber = true
 				vim.opt.cursorline = true
-				vim.opt.list = false
-				vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 				vim.opt.hlsearch = true
 				vim.opt.tabstop = 4
 				vim.opt.shiftwidth = 4
 				vim.opt.updatetime = 300
 				vim.opt.scrolloff = 10
 				vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-				vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-				vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+				vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+				vim.keymap.set('n', '[d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 				vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 				vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 				'';
-			plugins = with pkgs.vimPlugins; [
-				nvim-treesitter.withAllGrammars
+			plugins =  [
+			    {
+					plugin = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+					type = "lua";
+					config = builtins.readFile ./lua/treesitter.lua;
+				}
 				{
-					plugin = gitsigns-nvim;
+					plugin = pkgs.vimPlugins.gitsigns-nvim;
 					type = "lua";
 					config = '' require("gitsigns").setup() '';
 				}
 				{
-					plugin = nvim-lspconfig;
+					plugin = pkgs.vimPlugins.nvim-lspconfig;
 					type = "lua";
 					config = builtins.readFile ./lua/lsp.lua;
 				}
