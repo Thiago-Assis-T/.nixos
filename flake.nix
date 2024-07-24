96be0029c06a5dbfbd66a4c4d3845699a1ce1146
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-07-24 20:24:05 -0300

    aaaaa

--- flake.nix
+++ flake.nix
@@ -21,81 +21,62 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    inputs@{
-      nixpkgs,
-      disko,
-      home-manager,
-      ...
-    }:
-    {
-      nixosConfigurations = {
-        ThiagoServer = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./hosts/ThiagoServer/configuration.nix
-            disko.nixosModules.disko
-            home-manager.nixosModules.home-manager
-            inputs.stylix.nixosModules.stylix
-            (import ./overlays.nix)
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit inputs;
-                };
-                users.thiago = import ./home/server.nix;
-              };
-            }
-          ];
-        };
-        ThiagoDesktop = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./hosts/ThiagoDesktop/configuration.nix
-            disko.nixosModules.disko
-            home-manager.nixosModules.home-manager
-            inputs.stylix.nixosModules.stylix
-            (import ./overlays.nix)
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit inputs;
-                };
-                users.thiago = import ./home/desktop.nix;
-              };
-            }
-          ];
-        };
-        ThiagoLaptop = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./hosts/ThiagoLaptop/configuration.nix
-            disko.nixosModules.disko
-            home-manager.nixosModules.home-manager
-            inputs.stylix.nixosModules.stylix
-            (import ./overlays.nix)
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit inputs;
-                };
-                users.thiago = import ./home/laptop.nix;
-              };
-            }
-          ];
-        };
+  outputs = inputs@{ nixpkgs, disko, home-manager, ... }: {
+    nixosConfigurations = {
+      ThiagoServer = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/ThiagoServer/configuration.nix
+          disko.nixosModules.disko
+          home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          #(import ./overlays.nix)
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.thiago = import ./home/server.nix;
+            };
+          }
+        ];
+      };
+      ThiagoDesktop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/ThiagoDesktop/configuration.nix
+          disko.nixosModules.disko
+          home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          (import ./overlays.nix)
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.thiago = import ./home/desktop.nix;
+            };
+          }
+        ];
+      };
+      ThiagoLaptop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/ThiagoLaptop/configuration.nix
+          disko.nixosModules.disko
+          home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          (import ./overlays.nix)
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.thiago = import ./home/laptop.nix;
+            };
+          }
+        ];
       };
     };
+  };
 }
