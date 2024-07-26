e01f72ee63759c5ff8e75564275f18829cb2a198
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-07-26 13:21:46 -0300

    going to onedark

--- hosts/modules/stylix/default.nix
+++ hosts/modules/stylix/default.nix
@@ -1,38 +1,28 @@
 { pkgs, ... }:
 let
   inputImage = ../../../wallpapers/tokyo_rainy_road.jpg;
-  brightness = "-20";
-  contrast = "0";
-  fillColor = "black";
-in
-{
+  #brightness = "-20";
+  #contrast = "0";
+  #fillColor = "black";
+in {
   stylix = {
     enable = true;
     autoEnable = true;
     polarity = "dark";
-    image = pkgs.runCommand "dimmed-background.png" { } ''
-      ${pkgs.imagemagick}/bin/convert "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
-    '';
-    targets = {
-      nixvim = {
-        transparent_bg = {
-          main = true;
-          sign_column = true;
-        };
-      };
-    };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+    image = "${inputImage}";
+    #pkgs.runCommand "dimmed-background.png" { } ''
+    #${pkgs.imagemagick}/bin/convert "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
+    #'';
+    targets = { };
     cursor = {
       name = "BreezeX-RosePine-Linux";
       package = pkgs.rose-pine-cursor;
       size = 24;
     };
-    opacity = {
-      terminal = 0.8;
-    };
+    opacity = { terminal = 0.8; };
     fonts = {
-      sizes = {
-        terminal = 10;
-      };
+      sizes = { terminal = 10; };
       monospace = {
         package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
         name = "JetBrainsMono Nerd Font";
