6f936e5a24df14bcf37049713d5648d76349d863
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-03 00:22:07 -0300

    pathches from the flake

--- hosts/modules/dwl/package.nix
+++ hosts/modules/dwl/package.nix
@@ -3,5 +3,7 @@
   version = "v0.6";
   src = dwl-source;
   inherit patches;
+  buildInputs = previousAttrs.buildInputs
+    ++ [ pkgs.libdrm pkgs.fcft pkgs.pixman ];
   passthru.providedSessions = [ "dwl" ];
 })).override { configH = ./config.h; }
