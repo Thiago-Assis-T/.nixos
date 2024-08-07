1c6d34575d13ddb13c21f840e65b929f5506fa53
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-06 21:29:56 -0300

    printing

--- hosts/modules/printing/default.nix
+++ hosts/modules/printing/default.nix
@@ -1,18 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.printing.enable = true;
+  environment.systemPackages = with pkgs; [ system-config-printer ];
   services.avahi = {
     enable = true;
-    nssmdns = true;
+    nssmdns4 = true;
     openFirewall = true;
   };
-  ##hardware.printers = {
-  #  ensurePrinters = [{
-  #    name = "Epson_L3150";
-  #    location = "Home";
-  #    #deviceUri = "http://192.168.178.2:631/printers/Dell_1250c";
-  #    #model = "drv:///sample.drv/generic.ppd";
-  #    ppdOptions = { PageSize = "A4"; };
-  #  }];
-  #  ensureDefaultPrinter = "Epson_L3150";
-  #};
 }
