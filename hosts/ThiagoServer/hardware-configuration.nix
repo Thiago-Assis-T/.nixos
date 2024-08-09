28a899803303e8e2600ff44af9a9cacba7b54d76
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-09 15:30:46 -0300

    no compiling!

--- hosts/ThiagoServer/hardware-configuration.nix
+++ hosts/ThiagoServer/hardware-configuration.nix
@@ -32,8 +32,8 @@
     hostPlatform = {
       system = "x86_64-linux";
       config = "x86_64-unknown-linux-gnu";
-      gcc.arch = "skylake";
-      gcc.tune = "skylake";
+      #gcc.arch = "skylake";
+      #gcc.tune = "skylake";
     };
   };
   hardware.cpu.intel.updateMicrocode =
