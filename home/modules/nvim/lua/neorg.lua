96709cd0a08a624c065f43de0b2f1a70efa48f52
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-16 00:07:10 -0300

    some neorg shenanigans

--- home/modules/nvim/lua/neorg.lua
+++ home/modules/nvim/lua/neorg.lua
@@ -7,5 +7,15 @@ require("nvim-treesitter.configs").setup({
 require("neorg").setup({
 	load = {
 		["core.defaults"] = {},
+		["core.integrations.telescope"] = {
+			config = {
+				insert_file_link = {
+					show_title_preview = true,
+				},
+			},
+		},
 	},
 })
+
+vim.keymap.set("n", "<leader>nf", "<Plug>(neorg.telescope.find_norg_files)", { desc = "[N]eorg [F]iles" })
+vim.keymap.set("n", "<leader>nh", "<Plug>(neorg.telescope.search_headings)", { desc = "[N]eorg [H]eadings" })
