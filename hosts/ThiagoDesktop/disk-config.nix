7cc3ed2b3801b10256263721369cc77ba3995b1c
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-18 18:49:38 -0300

    now I am RAID0

--- hosts/ThiagoDesktop/disk-config.nix
+++ hosts/ThiagoDesktop/disk-config.nix
@@ -1,7 +1,7 @@
 {
   disko.devices = {
     disk = {
-      main = {
+      disk1 = {
         type = "disk";
         device = "/dev/nvme0n1";
         content = {
@@ -11,7 +11,7 @@
               priority = 1;
               name = "ESP";
               start = "1M";
-              end = "500M";
+              end = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
@@ -19,39 +19,64 @@
                 mountpoint = "/boot";
               };
             };
-            root = {
+            mdadm = {
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
-                # Subvolumes must set a mountpoint in order to be mounted,
-                # unless their parent is mounted
-                subvolumes = {
-                  # Subvolume name is different from mountpoint
-                  "/" = { mountpoint = "/"; };
-                  # Subvolume name is the same as the mountpoint
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                  };
-                  # Parent is not mounted so the mountpoint must be set
-                  "/nix" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/nix";
-                  };
-                  # Subvolume for the swapfile
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap = { swapfile.size = "35G"; };
-                  };
-                };
-                mountpoint = "/";
+                type = "mdraid";
+                name = "raid0";
+              };
+            };
+          };
+        };
+      };
+      disk2 = {
+        type = "disk";
+        device = "/dev/nvme1n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            mdadm = {
+              size = "100%";
+              content = {
+                type = "mdraid";
+                name = "raid0";
               };
             };
           };
         };
       };
     };
+    mdadm = {
+      raid0 = {
+        type = "mdadm";
+        level = 0;
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ]; # Override existing partition
+          # Subvolumes must set a mountpoint in order to be mounted,
+          # unless their parent is mounted
+          subvolumes = {
+            # Subvolume name is different from mountpoint
+            "/" = { mountpoint = "/"; };
+            # Subvolume name is the same as the mountpoint
+            "/home" = {
+              mountOptions = [ "compress=zstd" ];
+              mountpoint = "/home";
+            };
+            # Parent is not mounted so the mountpoint must be set
+            "/nix" = {
+              mountOptions = [ "compress=zstd" "noatime" ];
+              mountpoint = "/nix";
+            };
+            # Subvolume for the swapfile
+            "/swap" = {
+              mountpoint = "/.swapvol";
+              swap = { swapfile.size = "35G"; };
+            };
+          };
+          mountpoint = "/";
+        };
+      };
+    };
   };
 }
-
