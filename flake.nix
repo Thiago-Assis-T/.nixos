b82bd9d9b7ec2a776f3ab00908e5214e2c160da3
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-19 00:07:34 -0300

    wallpaper update

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
     };
     wallpaper = {
       url =
-        "https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/misc/od_current.png";
+        "https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/minimal/od_ometer.png";
+      #"https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/misc/od_current.png";
       #"https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/minimal/od_space01.png";
       #"https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/minimal/od_wave.png";
       #"https://raw.githubusercontent.com/Narmis-E/onedark-wallpapers/main/misc/od_waves.jpg";
