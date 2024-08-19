1c9a5399f1516cb1f4e165201e1cf7d4dc05a43f
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-19 13:22:16 -0300

    update

--- hosts/modules/loginManager/default.nix
+++ hosts/modules/loginManager/default.nix
@@ -2,7 +2,10 @@
   programs.regreet = {
     enable = true;
     settings = {
-      background.path = inputs.wallpaper;
+      background = {
+        path = inputs.wallpaper;
+        fit = "Fill";
+      };
       gtk = { application_prefer_dark_theme = true; };
     };
   };
