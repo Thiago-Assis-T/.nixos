fb68f91edd941b35c52bec392813fe66231c4629
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-08-31 00:10:01 -0300

    aaaa

--- hosts/ThiagoServer/modules/nextcloud/default.nix
+++ hosts/ThiagoServer/modules/nextcloud/default.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }: {
   networking.firewall.allowedTCPPorts = [ 80 443 5432 ];
   environment.etc."nextcloud-admin-pass".text = "4Hg428u$@";
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.collabora = {
+      image = "collabora/code:latest";
+      ports = [ "9980:9980" ];
+      environment = {
+        extra_params = "--o:ssl.enable=false --o:ssl.termination=false";
+      };
+      extraOptions = [ "--cap-add" "MKNOD" ];
+    };
+  };
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud29;
