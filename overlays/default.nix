27b735d97c0f0eb60baba3c944565adc551cad83
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-09-13 12:05:21 -0300

    lets try again

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,55 +1,52 @@
 { ... }: {
   nixpkgs.overlays = [
-    #    (final: prev: {
-    #      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-    #        (pyfinal: pyprev: {
-    #          numpy = pyprev.numpy.overridePythonAttrs (oldAttrs: {
-    #            postPatch = ''
-    #              rm numpy/core/tests/test_cython.py
-    #              rm numpy/core/tests/test_umath_accuracy.py
-    #              rm numpy/core/tests/test_*.py
-    #            '';
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #            disabledTests = [
-    #              "test_math"
-    #              "test_umath_accuracy"
-    #              "test_validate_transcendentals"
-    #            ];
-    #          });
-    #          meson = pyprev.scipy.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #          scipy = pyprev.scipy.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #          pandas = pyprev.pandas.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #          eventlet = pyprev.eventlet.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #          aiohttp = pyprev.aiohttp.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false;
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #          websockets = pyprev.websockets.overridePythonAttrs (oldAttrs: {
-    #            doCheck = false; # test hanged
-    #            doInstallCheck = false;
-    #            dontCheck = true;
-    #          });
-    #        })
-    #      ];
-    #    })
+    (final: prev: {
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+        (pyfinal: pyprev: {
+          numpy = pyprev.numpy.overridePythonAttrs (oldAttrs: {
+            postPatch = ''
+              rm numpy/core/tests/test_cython.py
+              rm numpy/core/tests/test_umath_accuracy.py
+              rm numpy/core/tests/test_*.py
+              substituteInPlace pyproject.toml \
+              	--replace-fail "meson-python>=0.15.0,<0.16.0" "meson-python"
+            '';
+            doCheck = false;
+            doInstallCheck = false;
+            dontCheck = true;
+            disabledTests = [
+              "test_math"
+              "test_umath_accuracy"
+              "test_validate_transcendentals"
+            ];
+          });
+          scipy = pyprev.scipy.overridePythonAttrs (oldAttrs: {
+            doCheck = false;
+            doInstallCheck = false;
+            dontCheck = true;
+          });
+          pandas = pyprev.pandas.overridePythonAttrs (oldAttrs: {
+            doCheck = false;
+            doInstallCheck = false;
+            dontCheck = true;
+          });
+          eventlet = pyprev.eventlet.overridePythonAttrs (oldAttrs: {
+            doCheck = false;
+            doInstallCheck = false;
+            dontCheck = true;
+          });
+          aiohttp = pyprev.aiohttp.overridePythonAttrs (oldAttrs: {
+            doCheck = false;
+            doInstallCheck = false;
+            dontCheck = true;
+          });
+          websockets = pyprev.websockets.overridePythonAttrs (oldAttrs: {
+            doCheck = false; # test hanged
+            doInstallCheck = false;
+            dontCheck = true;
+          });
+        })
+      ];
+    })
   ];
 }
