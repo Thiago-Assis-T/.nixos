80ceafe10af5e49aa0f9c715e747d0a51386134c
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-10-04 20:33:53 -0300

    printing and update... cant wait for that dkjson stupidity

--- hosts/modules/printing/default.nix
+++ hosts/modules/printing/default.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
   services.system-config-printer.enable = true;
   programs.system-config-printer.enable = true;
+  hardware.sane.enable = true;
+  users.users.thiago.extraGroups = [ "scanner" "lp" ];
+  environment.systemPackages =with pkgs; [
+    simple-scan
+  ];
   services.printing = {
     enable = true;
     webInterface = false;
