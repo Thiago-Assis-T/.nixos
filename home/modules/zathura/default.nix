2ed2e8ad4b7cad5f45384cf693a7c5938ba6d10b
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2024-10-05 10:40:27 -0300

    adding the files

---
+++ home/modules/zathura/default.nix
@@ -0,0 +1,17 @@
+{ config, lib, ... }:
+let cfg = config.programs.my-git;
+
+in {
+
+  options.programs.my-zathura = {
+    enable = lib.mkEnableOption (lib.mdDoc "my-zathura");
+  };
+  config = lib.mkIf cfg.enable {
+    programs.zathura = {
+      enable = true;
+      extraConfig = builtins.readFile ./zathurarc;
+
+    };
+
+  };
+}
