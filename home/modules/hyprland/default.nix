3bf9f20c4e0a9658d85834416908bed379be3b65
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-03-18 22:08:20 -0300

    hyprland and waybar

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -12,7 +12,7 @@
 
       general = {
         gaps_in = 5;
-        gaps_out = 20;
+        gaps_out = 10;
 
         border_size = 2;
 
--- home/modules/waybar/default.nix
+++ home/modules/waybar/default.nix
@@ -10,7 +10,55 @@
       			'';
 
     settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        #height = 42;
+        # width= 1280; # Waybar width
+        spacing = 4;
+        # Gaps between modules (4px)
+        # Choose the order of the modules
+        modules-left = [
+          #"custom/nixos"
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          "idle_inhibitor"
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "temperature"
+          #"hyprland/language"
+          "battery"
+          "battery#bat2"
+          "clock"
+        ];
+        # Modules configuration
 
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          warp-on-scroll = false;
+          format = "{name}";
+          format-icons = {
+            urgent = "";
+            active = "";
+            default = "";
+          };
+        };
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = " ";
+            deactivated = " ";
+          };
+        };
+      };
     };
   };
 }
