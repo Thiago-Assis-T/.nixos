088c9d577e064afd4519a39b9f0d170c0e5ca259
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-03-23 19:14:08 -0300

    something

--- hosts/ThiagoLaptop/configuration.nix
+++ hosts/ThiagoLaptop/configuration.nix
@@ -69,6 +69,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   #services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
