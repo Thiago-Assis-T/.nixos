93d1c3eab94847eb80dd4a6a862795b287e09855
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-04-04 00:00:34 -0300

    dasdot...

--- hosts/modules/dashdot/default.nix
+++ hosts/modules/dashdot/default.nix
@@ -5,11 +5,17 @@
       image = "mauricenino/dashdot:latest";
       privileged = true;
       ports = [
-        "80:3001"
+        "3001:3001"
       ];
       volumes = [
         "/:/mnt/host:ro"
       ];
+      environment = {
+        DASHDOT_WIDGET_LIST = "os,cpu,storage,ram,network";
+        DASHDOT_PAGE_TITLE = "System Monitoring";
+        DASHDOT_ALWAYS_SHOW_PERCENTAGES = "true";
+        DASHDOT_OVERRIDE_OS = "NixOS 25.05 (Warbler)";
+      };
     };
   };
 }
