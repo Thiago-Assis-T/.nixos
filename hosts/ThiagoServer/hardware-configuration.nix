e67da5d5b10b1cbca7916360b506985170ee6f28
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-04-05 20:44:54 -0300

    update

--- hosts/ThiagoServer/hardware-configuration.nix
+++ hosts/ThiagoServer/hardware-configuration.nix
@@ -137,10 +137,10 @@ in
         intel-media-driver
         intel-vaapi-driver
         vaapiVdpau
-        #intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
         # OpenCL support for intel CPUs before 12th gen
         # see: https://github.com/NixOS/nixpkgs/issues/356535
-        intel-compute-runtime-legacy1
+        #intel-compute-runtime-legacy1
         vpl-gpu-rt # QSV on 11th gen or newer
         intel-media-sdk # QSV up to 11th gen
 
