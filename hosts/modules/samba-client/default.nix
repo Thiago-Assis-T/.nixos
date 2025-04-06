20607e538e7ad97030e500f687785301398d728a
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-04-06 13:48:29 -0300

    keep it mounted

--- hosts/modules/samba-client/default.nix
+++ hosts/modules/samba-client/default.nix
@@ -7,7 +7,7 @@
     fsType = "cifs";
     options =
       let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+        automount_opts = "user,users";
 
       in
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
