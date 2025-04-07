0a151f061cf9e0c0984422b631a3f02c66fa1d6a
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2025-04-06 23:26:14 -0300

    something

--- hosts/modules/reverse-proxy/default.nix
+++ hosts/modules/reverse-proxy/default.nix
@@ -2,26 +2,19 @@
 {
   virtualisation.oci-containers.containers = {
     reverse-proxy = {
-      #image = "jc21/nginx-proxy-manager:latest";
-      image = "traefik:latest";
+      image = "jc21/nginx-proxy-manager:latest";
       cmd = [
-        "--api.insecure=true"
-        "--providers.docker"
       ];
       ports = [
         "80:80"
         "443:443"
-        "81:8080"
+        "81:81"
       ];
       volumes = [
-        #"/home/thiago/reverse-proxy:/data"
+        "/home/thiago/reverse-proxy:/data"
         "/var/run/podman/podman.sock:/var/run/docker.sock"
-        #"/home/thiago/reverse-proxy/letsencrypt:/etc/letsencrypt"
+        "/home/thiago/reverse-proxy/letsencrypt:/etc/letsencrypt"
       ];
-      environment = {
-        PUID = "1000";
-        GUID = "1000";
-      };
     };
   };
 
